572b0a037d97979c37d09120ba7d65fa36f9011b
Ilya <ilya.verevkin.2018@mail.ru>
2025-09-21 14:19:56 +0700

    switch case в консольном приложении

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -7,5 +7,54 @@ using Logic;
 
 namespace ConsoleUI
 {
-    
-}
+    class Program
+    {
+        static TeacherManager manager = new TeacherManager();
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("1. Добавить преподавателя");
+                Console.WriteLine("2. Показать всех преподавателей");
+                Console.WriteLine("3. Удалить преподавателя");
+                Console.WriteLine("4. Изменить преподавателя");
+                Console.WriteLine("5. Сгруппировать по предметам");
+                Console.WriteLine("6. Найти по стажу");
+                Console.WriteLine("7. Выход");
+                Console.Write("Выберите: ");
+
+                var choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        AddTeacher();
+                        break;
+                    case "2":
+                        ShowAllTeachers();
+                        break;
+                    case "3":
+                        DeleteTeacher();
+                        break;
+                    case "4":
+                        UpdateTeacher();
+                        break;
+                    case "5":
+                        GroupBySubject();
+                        break;
+                    case "6":
+                        FilterByExperience();
+                        break;
+                    case "7":
+                        return;
+                    default:
+                        Console.WriteLine("Неверный выбор");
+                        break;
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+    }
